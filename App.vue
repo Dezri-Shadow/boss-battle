0ae9f5581466bf53ab27b7b6a392168fa846cb14
Dezri-Shadow <69326414+Dezri-Shadow@users.noreply.github.com>
2020-08-06 17:28:23 -0400

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,110 @@
+<template>
+  <div id="app">
+
+    <button
+      type="button"
+      class="btn"
+      @click="showCharacterSheet"
+    >
+      Open Character Sheet
+    </button>
+    <img @click="moveRight" :style="cssVars" class="character" id="hero" :src="heroUrl" />
+    <img class="boss" id="boss" :src="bossUrl"/>
+
+    <characterSheet
+      v-show="isCharacterSheetVisible"
+      @close="closeCharacterSheet"
+    ></characterSheet>
+  </div>
+
+</template>
+
+<script>
+import characterSheet from './components/characterSheet.vue';
+
+export default {
+  name: 'App',
+  components: {
+    characterSheet
+  },
+
+  data () {
+      return {
+        isCharacterSheetVisible: false,
+        pos: -616,
+        heroUrl: require( "./assets/hero1.png" ),
+        bossUrl: require( './assets/dragon.png' )
+      };
+  },
+  computed: {
+    cssVars() {
+      return {
+        '--pos': this.pos + 'px'
+      }
+    }
+  },
+  methods: {
+      showCharacterSheet() {
+        this.isCharacterSheetVisible = true;
+      },
+      closeCharacterSheet() {
+        this.isCharacterSheetVisible = false;
+      },
+      moveRight() {
+        this.pos += 84; 
+        if(this.pos == 560){
+          alert('fight');
+        }
+      }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  margin-left: 60px;
+  margin-bottom: 60px;
+  background-image: url('./assets/dungeon.jpg');
+  min-height: 1000px;
+  background-repeat: no-repeat;
+  max-width: 1500px;
+}
+
+.btn {
+    padding: 10px;
+    background-color: #00599a;
+    margin: 10px;
+    border: solid 2px #000;
+    border-radius: 10%;
+    color: #fff;
+    position: absolute;
+
+}
+.btn:hover{
+  background-color: #fff;
+  color: #00599a;
+  border-color: #00599a;
+  font-weight: bold;
+}
+.btn:focus{
+  outline: -webkit-focus-ring-color auto 0px;
+}
+
+.character {
+  position: relative;
+  top: 510px;
+  left: var(--pos);
+}
+
+.boss {
+  position: relative;
+  top: 516px;
+  right: -520px;
+}
+</style>
